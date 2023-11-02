b798bd4042bc91e8486e9750c4e48c0dd6ae4f0a
Victor Frolov <vgf65@yandex.ru>
2023-11-02 19:38:34 +0300

    Add files via upload
    last version

--- phon.py
+++ phon.py
@@ -11,54 +11,112 @@ def show_menu():
     choice = int(input("Введите номер строки меню: " ))
     #print  ("Меню: ", choice)
     return choice
-#------------------------------------------        
-def work_with_phonebook ():
-    choice = show_menu()
-    print ("Вы выбрали  меню: ", choice)
 
-#-------------------------------------------------------
-    phone_book = open('phonebook.txt', mode='rt') #открытие файла
-    #print(phone_book.read()) 
-    # phone_book.close()
-    
-    while (choice!=7):
-            if choice==1:
-                print("-----------------------")
-                print("Телефонный справочник:")  
-                print( phone_book.read())  
-                print("-----------------------")
-                phone_book.close() 
-                #choice = show_menu()
-                work_with_phonebook ()
-            elif choice==2:
-                last_name= input('Поиск по Фамилии ')
-                #print(find_by_lastname, (phone_book, last_name))
-                read_txt()
-                print(find_by_lastname, (phone_book, last_name))
-                phone_book.close() 
-            elif choice==3:
-                last_name= input('lastname ')
-                new_number = input("new_number")
-                print(change_number(phone_book, last_name, new_number))
-            elif choice==4:
-                lastname=input('lastname ')
-                print (delete_by_lastname(phone_book, last_name))
-            elif choice==5:
-                number=input('number ')
-                print (find_by_number (phone_book, number))
-            elif choice==6:
-                user_data=input("new data ")
-                add_user (phone_book, user_data)
-                write_txt('phonebook.txt, phone_book ')  
-               
-#--------------------------------------------   
-#Lne_book = open('phonebook.txt', mode='rt') #открытие файла
-    # phone_book = open('phonebook.txt', 'r') #открытие файла
-    # print(phone_book.read()) 
-    # phone_book.close()
-def read_txt (phone_book):
-    phone_book [filds]
+
+def read_txt (filename):
+    phone_book = []
     filds = ["Фамилия", "Имя", "Телефон", "Примечания"]
 
-work_with_phonebook ()
+    with open(filename,'r',encoding='utf-8') as f:
+        for line in f:
+            record=dict(zip(filds,line.split(',')))
+            phone_book.append(record)
+    f.close()
+    return phone_book
+
+def write_txt(filename, phone_book):
+    with open(filename,'w',encoding='utf-8') as f:
+        for i in range(len(phone_book)):
+            f.write(phone_book[i].get('Фамилия')+','+phone_book[i].get('Имя')+','+phone_book[i].get('Телефон')+','+phone_book[i].get('Примечания'))
+    f.close()
+
+def find_by_lastname(phone_book, last_name):
+    key = []
+    for i in range(len(phone_book)):
+        if phone_book[i].get('Фамилия')==last_name:
+            key.append(phone_book[i])
+    return (key)
+
+def find_by_number (phone_book, number):
+    key = []
+    for i in range(len(phone_book)):
+        if phone_book[i].get('Телефон')==number:
+            key.append(phone_book[i])
+    return (key)
+
+def change_number(phone_book, last_name, new_number):
+    for i in range(len(phone_book)):
+        if phone_book[i].get('Фамилия')==last_name:
+            phone_book[i]['Телефон']=new_number
+    write_txt('phonebook1.txt',phone_book)
+    return phone_book
+
+def delete_by_lastname(phone_book, last_name):
+    a =0
+    for i in range(0,len(phone_book)):
+        if phone_book[i].get('Фамилия')==last_name:
+            a = i
+    phone_book.pop(a)
+    write_txt('phonebook1.txt',phone_book)
+    return phone_book
+
+def add_user(phone_book, user_data):
+    filds = ["Фамилия", "Имя", "Телефон", "Примечания"]
+    record=dict(zip(filds,user_data.split(',')))
+    record['Примечания']+='\n'
+    phone_book.append(record)
+    write_txt('phonebook1.txt',phone_book)
+    return phone_book
+
+def print_line(line):
+    print(line.get('Фамилия')+' '+line.get('Имя')+' '+line.get('Телефон')+' '+line.get('Примечания'))
+
+#------------------------------------------  
+
+
+#def work_with_phonebook ():
+choice = show_menu()
+print ("Вы выбрали  меню: ", choice)
+
+#-------------------------------------------------------
+phone_book = read_txt('phonebook1.txt')
+    #print(phone_book.read()) 
+    # phone_book.close()
+while (choice!=7):
+    if choice==1:
+        print("-----------------------")
+        print("Телефонный справочник:")  
+        for i in range(len(phone_book)):
+            print_line(phone_book[i])
+        print("-----------------------")
+    elif choice==2:
+        last_name= input('Поиск по Фамилии ')
+        rez=find_by_lastname(phone_book, last_name)
+        for i in range(len(rez)):
+            print_line(rez[i])
+    elif choice==3:
+        last_name= input('Введите фамилию: ')
+        new_number = input("Введите новый номер:")
+        phone_book=change_number(phone_book, last_name, new_number)
+        for i in range(len(phone_book)):
+            print_line(phone_book[i])
+    elif choice==4:
+        last_name=input('Удаление контакта по фамилии: ')
+        phone_book=delete_by_lastname(phone_book, last_name)
+        for i in range(len(phone_book)):
+            print_line(phone_book[i])
+    elif choice==5:
+        number=input('Введите номер для поиска: ')
+        rez=find_by_number (phone_book, number)
+        for i in range(len(rez)):
+            print_line(rez[i])
+    elif choice==6:
+        user_data=input("Новый контакт: ")
+        phone_book=add_user(phone_book, user_data)
+        for i in range(len(phone_book)):
+            print_line(phone_book[i])
+    choice = show_menu()
+    print ("Вы выбрали  меню: ", choice)
+       
+#work_with_phonebook ()
 
